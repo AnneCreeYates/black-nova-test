a0be25a3cc50ef734cb6e220fb780f3e502786bb
Anna Edmund <annecreeyates@gmail.com>
2022-10-15 22:45:26 +0100

    removed all the gulp, dependency files and sass -- made trouble; added the nav.js file and reset the styles.css

---
+++ nav.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger-bars")
+const mainNavToggle = document.querySelector(".main-nav")
+const mainNavItem = document.querySelectorAll(".main-nav__item")
+
+mainNavToggle.addEventListener("click", _ => {
+    document.body.classList.toggle("is-open")
+
+})
